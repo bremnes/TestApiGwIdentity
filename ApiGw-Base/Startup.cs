652610215166fca7146657fc45b2d1734b86510f
Lars Kristian Bremnes <bremnes@gmail.com>
2018-08-04 01:24:02 +0200

    Added identityserver. Must be authenticated to get to web app through api gw.

--- ApiGw-Base/Startup.cs
+++ ApiGw-Base/Startup.cs
@@ -1,11 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using CacheManager.Core;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
@@ -45,7 +39,9 @@ namespace OcelotApiGw
                     x.RequireHttpsMetadata = false;
                     x.TokenValidationParameters = new Microsoft.IdentityModel.Tokens.TokenValidationParameters()
                     {
-                        ValidAudiences = new[] { "orders", "basket", "locations", "marketing", "mobileshoppingagg", "webshoppingagg" }
+                        // TODO: figure out best practice
+                        ValidateAudience = false,
+                        //ValidAudiences = new[] { "externalCrmSystem" }
                     };
                     x.Events = new Microsoft.AspNetCore.Authentication.JwtBearer.JwtBearerEvents()
                     {
